ca6d1bfa6431443d06e7a79600acac5c55944b52
Zannatul Naim <zannatulnaim09@gmail.com>
2022-07-02 22:55:40 +0600

    open a text file for read

--- Files/Open.cpp
+++ Files/Open.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main() {
     ifstream file;
-    file.open("test.txt");
+    file.open("test.txt");  // opening test.txt file for read
     cout << "Reading from file : " << endl;
-    string data, line;
-    file>>data;
-    getline(file, line);
+    string data, line;   
+    file>>data;  // a string written into data from file
+    getline(file, line);  // a string line written into line from file
     cout << data << endl;
     cout << line << endl;
     file.close();
